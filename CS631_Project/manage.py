e69f40ff51bac4f3807e2a20e94bb10742751665
Nick McCaffery <55106363+jango2447@users.noreply.github.com>
2025-12-03 19:48:35 -0500

    Add files via upload

--- CS631_Project/manage.py
+++ CS631_Project/manage.py
@@ -1,10 +1,16 @@
 from flask.cli import FlaskGroup
 from CS631_Project.app import create_app, db
 from flask_migrate import Migrate
-
+from flask_sqlalchemy import SQLAlchemy
 app = create_app()
 migrate = Migrate(app, db)
 cli = FlaskGroup(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL',
+    'postgresql://postgres:password@localhost:5432/mydb'
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     cli()
